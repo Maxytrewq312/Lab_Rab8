090876e219dcf39c19016da414b9b0fbc5b24da4
Максим <@DESKTOP-TSPOI6V>
2020-11-30 13:16:37 +0300

    Добавьте файлы проекта.

---
+++ Задание_1/Задание_1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <time.h>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int a, b;
+    int mas[10];
+    srand((unsigned)time(0));
+    cout << "Начальный массив" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        mas[i] = rand() % 9 + 1;
+        cout << mas[i] << " ";
+    }
+    cout << "\nИнтервал от a до b включительно...\n";
+    cout << "Введите a ";
+    cin >> a;
+    cout << "Введите b ";
+    cin >> b;
+    for (int i = 0; i < 10; i++)
+    {
+        if ((mas[i] >= a) && (mas[i] <= b))
+            mas[i] = 0;
+    }
+    cout << "Итоговый массив: \n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << mas[i] << " ";
+    }
+    system("pause");
+}
---
+++ Задание_2/Задание_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <time.h>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int a, b;
+    int mas[10];
+    srand((unsigned)time(0));
+    cout << "Начальный массив" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        mas[i] = rand() % 10;
+        cout << mas[i] << " ";
+    }
+    cout << "\nИнтервал от a до b включительно...\n";
+    cout << "Введите a ";
+    cin >> a;
+    cout << "Введите b ";
+    cin >> b;
+    if (a < b)
+        swap(b, a);
+    cout << "Не попадают под интервал элементы: ";
+    for (int i = 0; i < 10; i++)
+    {
+        if ((mas[i] >= b) && (mas[i] <= a))
+        {
+            i++;
+        }
+        else
+        {
+            cout << mas[i] << " ";
+        }
+    }
+    system("pause");
+}
